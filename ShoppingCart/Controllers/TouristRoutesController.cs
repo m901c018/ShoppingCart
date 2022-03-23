f3bb32e050de5e792ab0f4f91b9f571b27951816
陳建穎 <m901c018@gmail.com>
2022-03-23 15:15:19 +0800

    1.初版購物車開發中

---
+++ ShoppingCart/Controllers/TouristRoutesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShoppingCart.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TouristRoutesController : ControllerBase
+    {
+        private ITouristRouteRepository _TouristRouteRepository;
+
+        public TouristRoutesController(ITouristRouteRepository touristRouteRepository)
+        {
+            _TouristRouteRepository = touristRouteRepository;
+        }
+
+        //public IActionResult GetTouristRoutes()
+        //{
+
+        //}
+        [HttpGet]
+        public IActionResult GetTouristRoutes()
+        {
+            var routes = _TouristRouteRepository.GetTouristRoutes();
+            return Ok(routes);
+        }
+
+        //api/TouristRoutes/gTouristRoutesId
+        [HttpGet("{gTouristRoutesId:Guid}")]
+        public IActionResult GetTouristRouteById(Guid gTouristRoutesId)
+        {
+            return Ok(_TouristRouteRepository.GetTouristRoute(gTouristRoutesId));
+        }
+    }
+}
---
+++ ShoppingCart/Services/ITouristRouteRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShoppingCart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingCart.Services
+{
+    public interface ITouristRouteRepository
+    {
+        IEnumerable<TouristRoute> GetTouristRoutes();
+        TouristRoute GetTouristRoute(Guid TouristRouteId);
+    }
+}
